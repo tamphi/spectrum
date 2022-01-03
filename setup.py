ab62d4a7669fa39dcf87b6db43538c18ec558b3c
Tam Phi <tamphi2209@gmail.com>
2022-01-03 17:33:04 -0500

    python3

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 
 from setuptools import setup, find_packages
-#from distutils.core import setup
+
 
 setup(
-  name = 'spectrum',
-  version = '0.1',
+  name = "spectrum",
+  version = "0.1",
   packages = find_packages(),
   # scripts = ['say_hello.py'],
 
